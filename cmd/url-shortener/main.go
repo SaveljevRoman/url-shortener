8aeab412d55befd929794277d78c66d0f5c992e1
roman <saveljev.roman@gmail.com>
2024-05-30 21:57:23 +0300

    config

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 	// TODO: config: cleanenv
 	// TODO: logger: slog
 	// TODO: storage: sqlite
